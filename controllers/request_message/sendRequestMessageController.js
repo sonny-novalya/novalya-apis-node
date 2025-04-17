76b56f5b74b0899a7a6e16997f9219bf6a70c451
Sachin Kumar <geek.sachin95@gmail.com>
2025-04-17 23:45:51 +0530

    changes

--- controllers/request_message/sendRequestMessageController.js
+++ controllers/request_message/sendRequestMessageController.js
@@ -115,7 +115,8 @@ self.updateSendRequestMessage = async (req, res) => {
 self.getAllSendRequestMessages = async (req, res) => {
   try {
     const user_id = req.authUser;
-
+    console.log('user_id-118', user_id);
+    
     SendRequestMessage.findOne({
       where: {
         user_id: user_id,
