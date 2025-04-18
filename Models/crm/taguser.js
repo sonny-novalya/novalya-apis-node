1e2c9cd075f8c8db7c379b33bda21436daa693d2
Sachin Kumar <geek.sachin95@gmail.com>
2025-04-18 16:43:56 +0530

    changes

--- Models/crm/taguser.js
+++ Models/crm/taguser.js
@@ -1,62 +1,65 @@
 module.exports = (sequelize, DataTypes) => {
   const taggedUser = sequelize.define(
     "taggedusers",
-    {
-      user_id: {
-        type: DataTypes.INTEGER,
-      },
-      stage_id: {
-        type: DataTypes.INTEGER,
-      },
-      fb_name: {
-        type: DataTypes.STRING,
-      },
-      fb_image_id: {
-        type: DataTypes.STRING,
-      },
-      fb_user_id: {
-        type: DataTypes.STRING,
-        allowNull: true,
+    // {
+    //   user_id: {
+    //     type: DataTypes.INTEGER,
+    //   },
+    //   stage_id: {
+    //     type: DataTypes.INTEGER,
+    //   },
+    //   // fb_name: {
+    //   //   type: DataTypes.STRING(1000),
+    //   //   allowNull: true,
+    //   //   charset: 'utf8mb4',
+    //   //   collate: 'utf8mb4_unicode_ci',
+    //   // },
+    //   fb_image_id: {
+    //     type: DataTypes.STRING(500),
+    //   },
+    //   fb_user_id: {
+    //     type: DataTypes.STRING,
+    //     allowNull: true,
 
-      },
-      numeric_fb_id: {
-        type: DataTypes.STRING,
-        allowNull: true,
+    //   },
+    //   numeric_fb_id: {
+    //     type: DataTypes.STRING,
+    //     allowNull: true,
 
-      },
-      profile_pic: {
-        type: DataTypes.STRING,
-      },
-      is_primary: {
-        type: DataTypes.INTEGER,
-      },
-      user_note: {
-        type: DataTypes.TEXT,
-      },
-      tag_id: {
-        type: DataTypes.STRING,
-      },
-      is_e2ee: {
-        type: DataTypes.INTEGER,
-      },
-      fb_user_e2ee_id:{
-        type: DataTypes.STRING,
-        allowNull: true,
-        defaultValue: null,
-      }
-    },
+    //   },
+    //   profile_pic: {
+    //     type: DataTypes.TEXT,
+    //   },
+    //   is_primary: {
+    //     type: DataTypes.INTEGER,
+    //   },
+    //   user_note: {
+    //     type: DataTypes.TEXT,
+    //   },
+    //   tag_id: {
+    //     type: DataTypes.STRING,
+    //   },
+    //   is_e2ee: {
+    //     type: DataTypes.INTEGER,
+    //   },
+    //   fb_user_e2ee_id:{
+    //     type: DataTypes.STRING,
+    //     allowNull: true,
+    //     defaultValue: null,
+    //   }
+    // },
 
-    {
-      timestamps: true,
-    }
+    // {
+    //   timestamps: true,
+    // }
   );
 
-  taggedUser.sync({ alter: true })
-  .then(() => {
-    console.log('User table created or updated successfully');
-  })
-  .catch(err => {
-    console.error('Error creating or updating User table:', err);
-  });
+  // taggedUser.sync({ alter: true })
+  // .then(() => {
+  //   console.log('User table created or updated successfully');
+  // })
+  // .catch(err => {
+  //   console.error('Error creating or updating User table:', err);
+  // });
   return taggedUser;
 };
