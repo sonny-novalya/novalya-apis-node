c3e3eb59d3734083b82d6eba6d8202bfeadef06e
Sachin Kumar <geek.sachin95@gmail.com>
2025-05-13 22:51:20 +0530

    brevo api implement

--- controllers/settings/SettingController.js
+++ controllers/settings/SettingController.js
@@ -6,10 +6,14 @@ const db = require("../../Models/crm");
 const Response = require("../../helpers/response");
 const { checkAuthorization, Qry } = require("../../helpers/functions");
 require("dotenv").config();
+const axios = require("axios");
+const brevoKey = process.env.BRAVO_KEY;
+
+const BREVO_API_URL = "https://api.brevo.com/v3/contacts";
 
 self.getEnckeysOld = async (req, res) => {
   try {
-    const key = "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"; // Original key
+    const key = process.env.BRAVO_KEY; // Original key
     const bravoEncodedKey = Buffer.from(key).toString("base64"); // Base64 encode
 
     return Response.resWith202(res, "success", {"br_key": bravoEncodedKey});
@@ -81,7 +85,7 @@ self.saveUtmData = async (req, res) => {
   }
 };
 
-self.updateCapturePage = async (req, res) => {
+self.updateCapturePageOld = async (req, res) => {
   try {
     const { email, first_name, last_name, plan, plan_period, sponsor, country, plan_id } = req.body;
 
@@ -125,6 +129,124 @@ self.updateCapturePage = async (req, res) => {
     return Response.resWith422(res, "something went wrong");
   }
 };
+
+self.updateCapturePage = async (req, res) => {
+  try {
+    const {
+      email,
+      first_name,
+      last_name,
+      plan,
+      plan_period,
+      sponsor,
+      country,
+      plan_id,
+      currency = "",
+      plan_status = "Lead",
+      plan_amount = "",
+      invoice_paid = "",
+      plan_status_update_date = new Date().toISOString().split("T")[0],
+      sms = "",
+      username = "",
+      language = "en-US",
+      utm_data = {},
+      reseller = "Novalya",
+      affiliate_pro = ""
+    } = req.body;
+
+    if (!email || !first_name) {
+      return Response.resWith422(res, "Email and First Name are required");
+    }
+
+    const emailExists = await Qry(`SELECT 1 FROM usersdata WHERE email = ? and subscription_status='Active'  LIMIT 1`, [email]);
+
+    if (emailExists.length > 0) {
+      return Response.resWith422(res, "Email already exists");
+    }
+
+    const captureData = await Qry(
+      `SELECT sponsor FROM capture_page_data WHERE email = ? LIMIT 1`,
+      [email]
+    );
+
+    const sponsorData = await Qry(
+      `SELECT email FROM usersdata WHERE randomcode = ? LIMIT 1`,
+      [sponsor]
+    );
+
+    var sponsorEmail = (sponsorData && sponsorData[0]) ? sponsorData[0].email : '';
+
+    var sponsorCode = sponsor;
+
+    if (captureData.length > 0) {
+      if (sponsor === "NOVALYA") {
+        sponsorCode = captureData[0].sponsor;
+      }
+
+      await Qry(
+        `UPDATE capture_page_data 
+         SET sponsor = ?, plan_id = ?, plan = ?, plan_period = ? 
+         WHERE email = ? AND first_name = ?`,
+        [sponsorCode, plan_id, plan, plan_period, email, first_name]
+      );
+    } else {
+      await Qry(
+        `INSERT INTO capture_page_data (first_name, last_name, email, sponsor, plan_id, plan, plan_period, country) 
+         VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+        [first_name, last_name, email, sponsor, plan_id, plan, plan_period, country]
+      );
+    }
+
+    // Brevo API Call
+    await axios.post(
+      BREVO_API_URL,
+      {
+        email,
+        attributes: {
+          FIRSTNAME: first_name,
+          LASTNAME: last_name || "",
+          SMS: sms,
+          OPT_IN: "YES",
+          SPONSORID: sponsorCode,
+          COUNTRY: country || "",
+          SPONSOR_EMAIL: sponsorEmail,
+          USERNAME: username,
+          LANGUAGE: language,
+          NOVALYA_PLAN_NAME: plan,
+          NOVALYA_PLAN_PERIOD: plan_period,
+          NOVALYA_CURRENCY_CODE: currency,
+          NOVALYA_PLAN_STATUS: plan_status,
+          NOVALYA_PLAN_AMOUNT: plan_amount,
+          NOVALYA_INVOICE_PAID: invoice_paid,
+          NOVALYA_PLAN_STATUS_UPDATE_DATE: plan_status_update_date,
+          UTM_CAMPAIGN: utm_data.utm_campaign || "",
+          UTM_MEDIUM: utm_data.utm_medium || "",
+          UTM_SOURCE: utm_data.utm_source || "",
+          UTM_TERM: utm_data.utm_term || "",
+          UTM_CONTENT: utm_data.utm_content || "",
+          RESELLER: reseller,
+          AFFILIATE_PRO: affiliate_pro
+        },
+        listIds: [70], 
+        updateEnabled: true
+      },
+      {
+        headers: {
+          accept: "application/json",
+          "api-key": brevoKey,
+          "content-type": "application/json"
+        }
+      }
+    );
+
+    return Response.resWith202(res, "success");
+  } catch (error) {
+
+    console.error("Error occurred:", error);
+    return Response.resWith422(res, "something went wrong");
+  }
+};
+
 
 
 module.exports = self;
